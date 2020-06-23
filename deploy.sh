19268e1b4a3b47cb44ae627ece04177bf10490ad
root <root@server1.example.com>
2020-06-23 16:15:18 -0400

    updated the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 docker kill webserver > /dev/null 2>&1
 docker rm webserver > /dev/null 2>&1
 #docker build -t apacheam:v1
-docker-compose up -d
-#docker run -dit --name webserver apacheam:v1
+#docker-compose up -d
+docker run -dit --name webserver apacheam:v1
