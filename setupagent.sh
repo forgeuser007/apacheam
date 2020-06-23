9a97013ff81fea748e59cb9833b5f66bb69167fb
root <root@server1.example.com>
2020-06-23 14:55:16 -0400

    updated the hosts file

--- setupagent.sh
+++ setupagent.sh
@@ -2,7 +2,8 @@
 echo "Setting up web agent"
 echo "$PASSWORD" > /opt/.pwd
 chmod 400 /opt/.pwd
-echo "/opt/web_agents/apache24_agent/bin/agentadmin --s "/etc/httpd/conf/httpd.conf" http://openam1.example.com:8080/openam http://openam1.example.com:80 / webagent /opt/.pwd  --changeOwner --acceptLicence --forceInstall"
+echo "/opt/web_agents/apache24_agent/bin/agentadmin --s "/etc/httpd/conf/httpd.conf" http://openam1.example.com:8080/openam http://openam1.example.com:80 / webagent /opt/.pwd  --changeOwner --acceptLicence"
 
-/opt/web_agents/apache24_agent/bin/agentadmin --s "/etc/httpd/conf/httpd.conf" http://openam1.example.com:8080/openam http://openam1.example.com:80 / webagent /opt/.pwd --changeOwner --acceptLicence --forceInstall
+/opt/web_agents/apache24_agent/bin/agentadmin --s "/etc/httpd/conf/httpd.conf" http://openam1.example.com:8080/openam http://openam1.example.com:80 / webagent /opt/.pwd --changeOwner --acceptLicence 
 chown -R apache /opt/web_agents
+echo "192.168.1.181 openam1.example.com" >> /etc/hosts
